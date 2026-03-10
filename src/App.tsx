24abf0ae8942800d79887a2477b55f849d618998
yasir5405 <yasirnaseem5415@gmail.com>
2026-03-11 01:57:30 +0530

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Button } from "./components/ui/button";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button>Hello</Button>
+    </h1>
+  );
 };
 
 export default App;
