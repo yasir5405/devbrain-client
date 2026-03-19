e0787e98af2c8a57c7ac5184db5f4f8dc82f0370
yasir5405 <yasirnaseem5415@gmail.com>
2026-03-19 17:37:58 +0530

    Implement Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,46 @@
-import { navLinks } from "@/constants/constant";
+import { navLinks, toolLinks } from "@/constants/constant";
 import { Button } from "./ui/button";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
-import { Search } from "lucide-react";
+import { BrainCircuitIcon, Search } from "lucide-react";
 import { Kbd, KbdGroup } from "./ui/kbd";
 import { useNavigate } from "react-router-dom";
+import { ModeToggle } from "./ModeToggle";
+import { Separator } from "./ui/separator";
+import { useEffect, useState } from "react";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "./ui/command";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && e.key === "k") {
+        e.preventDefault();
+        setOpen((prev) => !prev);
+      }
+    };
+
+    window.addEventListener("keydown", down);
+
+    return () => window.removeEventListener("keydown", down);
+  }, []);
+
   return (
-    <nav className="w-full py-3 flex items-center justify-between border-b px-3 md:px-12 fixed top-0 left-0">
-      <div className="flex items-center justify-center w-fit gap-18 h-full">
-        <h1>DevBrain</h1>
+    <nav className="w-full py-3 flex items-center justify-between border-b dark:border-b-neutral-800 px-3 md:px-12 fixed top-0 left-0">
+      <div className="flex items-center justify-center w-fit gap-2 h-full">
+        <Button variant={"ghost"} onClick={() => navigate("/")}>
+          <BrainCircuitIcon className="size-4.25" />
+        </Button>
 
         <div className="flex items-center justify-center w-fit gap-1 h-full">
           {navLinks.map((link) => (
@@ -26,8 +56,8 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="flex items-center justify-center w-fit gap-10 h-full">
-        <InputGroup>
+      <div className="flex items-center justify-center w-fit gap-4 h-full">
+        <InputGroup onClick={() => setOpen(true)}>
           <InputGroupInput placeholder="Search tools or snippets..." />
 
           <InputGroupAddon>
@@ -41,11 +71,48 @@ const Navbar = () => {
           </InputGroupAddon>
         </InputGroup>
 
-        <div className="flex items-center justify-center w-fit gap-2 h-full">
-          <Button variant={"ghost"}>Sign in</Button>
-          <Button>Sign up</Button>
-          {/* <ModeToggle /> */}
-        </div>
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <Command>
+            <CommandInput placeholder="Search tools or roadmaps..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+
+              <CommandGroup heading="Navigation">
+                {navLinks.map((link) => (
+                  <CommandItem onSelect={() => navigate(link.href)}>
+                    {link.icon && <link.icon />}
+
+                    <span>{link.name}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+
+              <CommandSeparator />
+
+              <CommandGroup heading="Tools">
+                {toolLinks.map((link) => (
+                  <CommandItem onSelect={() => navigate(link.href)}>
+                    {link.icon && <link.icon />}
+                    <span>{link.name}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+
+            <div className="border-t px-3 py-2 text-xs text-muted-foreground flex items-center gap-2">
+              <Kbd>↵</Kbd>
+              Go to page
+            </div>
+          </Command>
+        </CommandDialog>
+
+        <Separator orientation="vertical" />
+
+        <ModeToggle />
+
+        <Separator orientation="vertical" />
+
+        <Button>Sign in</Button>
       </div>
     </nav>
   );
--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,9 +1,15 @@
 import type { navLinksInterface } from "@/types/navlinks.types";
-import { Code, Compass, Map, Zap } from "lucide-react";
+import { Code, Compass, Map, Zap, Braces, Binary, Key } from "lucide-react";
 
 export const navLinks: navLinksInterface[] = [
   { name: "Explore", href: "/explore", icon: Compass },
   { name: "Tools", href: "/tools", icon: Zap },
   { name: "Snippet", href: "/snippets", icon: Code },
   { name: "Roadmap", href: "/roadmap", icon: Map },
+];
+
+export const toolLinks: navLinksInterface[] = [
+  { name: "Json Formatter", href: "/tools/json-formatter", icon: Braces },
+  { name: "Base64 Encoder/Decode", href: "/tools/base64", icon: Binary },
+  { name: "JWT Decoder", href: "/tools/jwt-decoder", icon: Key },
 ];
